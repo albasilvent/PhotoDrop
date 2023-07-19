0bb0ca8ba23f2d2214d9cb968451e309ccbd5852
Joshue <joshueperez3@gmail.com>
2023-07-19 22:06:18 +0200

    login acabado y retoques

--- FRONTEND/photoDrop/src/components/Login.jsx
+++ FRONTEND/photoDrop/src/components/Login.jsx
@@ -3,15 +3,20 @@ import "../styles/Login.css";
 export function Login() {
     return (
         <main className="main">
-            <form className="form" action="">
-                <input type="email" placeholder="Introduce@tuEmail.com" />
-                <input type="password" placeholder="Contraseña" />
-                <button>Enviar</button>
-            </form>
-            <a href="#">¿Has olvidado la contraseña?</a>
-            <div>
-                <p>¿No tienes cuenta?</p>
-                <a href="#">Regístrate</a>
+            <div className="cajetin">
+                <h2 className="login">Login</h2>
+                <form className="form" action="">
+                    <input type="email" placeholder="Correo electrónico" />
+                    <input type="password" placeholder="Contraseña" />
+                    <button>Enviar</button>
+                </form>
+                <a className="olvidado" href="#">
+                    ¿Has olvidado la contraseña?
+                </a>
+                <div className="registrate">
+                    <p>¿No tienes cuenta?</p>
+                    <a href="#">Regístrate</a>
+                </div>
             </div>
         </main>
     );
