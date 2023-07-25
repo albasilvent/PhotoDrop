a38099c42c8d367e325bf68c23a83dc021027e93
albasilvent <albasilvent@gmail.com>
2023-07-25 20:42:59 +0200

    cosas nuevas

--- FRONTEND/photoDrop/src/App.jsx
+++ FRONTEND/photoDrop/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { AuthProvider } from "./contexts/auth-context";
 import { LoginPage } from "./components/appPages/LoginPage";
 import { ProfilePage } from "./components/appPages/ProfilePage";
+import { RegisterPage } from "./components/appPages/RegisterPage";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/search" element={<SearchPage />} />
                     <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
                     <Route path="/users/:id" element={<ProfilePage />} />
                 </Routes>
             </AuthProvider>
---
+++ FRONTEND/photoDrop/src/components/appPages/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../Header";
+import { Register } from "../Register";
+import { Footer } from "../Footer";
+
+export function RegisterPage() {
+    return (
+        <>
+            <Header />
+            <Register />
+            <Footer />
+        </>
+    );
+}
