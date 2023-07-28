16e8bd22d47b9c32e0bae964c2868b035ac364c7
Joshue <joshueperez3@gmail.com>
2023-07-29 00:05:07 +0200

    cambio componente Input

--- FRONTEND/photoDrop/src/components/Register.jsx
+++ FRONTEND/photoDrop/src/components/Register.jsx
@@ -1,37 +1,59 @@
 import "../styles/Register.css";
+import { Input } from "./Input";
 
 export function Register() {
     return (
         <main className="Page">
             <div className="container">
                 <div className="cajetin">
-                    <h2 className="titulo">¡Regístrate!</h2>
+                    <h2 className="titulo">Regístrate</h2>
                     <form className="form" action="">
-                        <input type="text" placeholder="Nombre" required />
-                        <input
+                        <Input
+                            name="text"
+                            type="text"
+                            placeholder="Nombre"
+                            required
+                        />
+                        <Input
+                            name="text"
                             type="text"
                             placeholder="Primer apellido"
                             required
                         />
-                        <input type="text" placeholder="Segundo Apellido" />
-                        <input type="email" placeholder="Email" required />
-                        <input
+                        <Input
+                            name="text"
+                            type="text"
+                            placeholder="Segundo apellido"
+                        />
+                        <Input
+                            name="email"
+                            type="email"
+                            placeholder="Email"
+                            required
+                        />
+                        <Input
+                            name="password"
                             type="password"
                             placeholder="Contraseña"
                             required
                         />
-                        <input type="date" required />
-                        <input type="text" placeholder="País" />
+                        <Input name="date" type="date" required />
+                        <Input name="text" type="text" placeholder="País" />
+
                         <div className="div-checkbox">
-                            <input type="checkbox" required />
-                            <label className="label" htmlFor="">
+                            <Input name="checkbox" type="checkbox" required />
+                            <label className="label" htmlFor="checkbox">
                                 Acepto los términos
                             </label>
                         </div>
-                        <button>Crear cuenta</button>
+                        <button className="boton" type="submit">
+                            Crear cuenta
+                        </button>
                     </form>
-                    <p>¿Tienes una cuenta?</p>
-                    <a href="#">¡Entra aquí!</a>
+                    <div className="log">
+                        <p>¿Tienes una cuenta?</p>
+                        <a href="#">Inicia sesión</a>
+                    </div>
                 </div>
             </div>
         </main>
