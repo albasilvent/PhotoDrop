ac6fd9241a4b14381089a9023b13de163c4b599b
Joshue <joshueperez3@gmail.com>
2023-08-04 01:55:28 +0200

    AddPost, faltan cosas y CSS

--- FRONTEND/photoDrop/src/App.jsx
+++ FRONTEND/photoDrop/src/App.jsx
@@ -7,7 +7,8 @@ import { LoginPage } from "./components/appPages/LoginPage";
 import { ProfilePage } from "./components/appPages/ProfilePage";
 import { RegisterPage } from "./components/appPages/RegisterPage";
 import { ValidateEmailPage } from "./components/appPages/ValidateEmailPage";
-import {PostDetailsPage} from "./components/appPages/PostDetailsPage"
+import { PostDetailsPage } from "./components/appPages/PostDetailsPage";
+import { AddPostPage } from "./components/appPages/AddPostPage";
 
 function App() {
     return (
@@ -19,11 +20,12 @@ function App() {
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/users/:id" element={<ProfilePage />} />
-                    <Route path="/posts/:id" element={<PostDetailsPage/>}/>
+                    <Route path="/posts/:id" element={<PostDetailsPage />} />
                     <Route
                         path="validate-email"
                         element={<ValidateEmailPage />}
                     />
+                    <Route path="/add-post" element={<AddPostPage />} />
                 </Routes>
             </AuthProvider>
         </>
